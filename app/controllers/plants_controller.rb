aa66ee12d60831a8714d5cd93eb150a3c5b23e8d
Kelly McGuffog <kellymcguffog@gmail.com>
2023-03-22 11:57:58 -0400

    commit tree 7a02ae49c10de190b4954ee29a4706e51869e160

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,22 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id:params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = Plant.find_by(id:params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "plant not found"}, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
